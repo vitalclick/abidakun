604a193f444b8a74ce602b81b3cd00981e375edd
Anthony Abidakun <toni@vitalclick.co.za>
2023-05-13 08:56:48 +0100

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,34 @@ import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '@/components/MDX'
 import '@/styles/globals.css'
+import { useEffect } from 'react';
+import { initGA, logPageView } from '../utils/googleAnalytics';
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
+
+  useEffect(() => {
+    initGA(); // Initialize Google Analytics
+    logPageView(); // Log the initial pageview
+
+    // Log pageview on route change
+    const handleRouteChange = (url) => {
+      logPageView();
+    };
+
+    // Event listener for route changes
+    window.addEventListener('routeChangeComplete', handleRouteChange);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
 
   return (
     <MDXProvider components={MDXComponents}>{getLayout(<Component {...pageProps} />)}</MDXProvider>
   )
 }
 
-export default MyApp
+export default MyApp;
